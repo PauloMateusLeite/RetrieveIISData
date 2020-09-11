741ab70df9c6786723d24a5bc57e3eef215c8b6c
Paulo Leite <E3074@itsector.pt>
2020-09-11 09:37:12 +0100

    mycommit

---
+++ IISDataRetriever/IISDataRetriever/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IISDataRetriever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //git add * && git commit -m "my commit" && git push origin master
+        }
+    }
+}
