b6ef4086a6ca23b575fb451a0c46aef21cd9a5c5
Paulo Leite <E3074@itsector.pt>
2020-09-11 10:32:05 +0100

    mycommit

--- IISDataRetriever/IISDataRetriever/Program.cs
+++ IISDataRetriever/IISDataRetriever/Program.cs
@@ -12,7 +12,30 @@ namespace IISDataRetriever
         static void Main(string[] args)
         {
 
-            //git add * && git commit -m "my commit" && git push origin master
+            //int iisNumber = 2;
+
+            using (ServerManager serverManager = new ServerManager())
+            {
+                foreach (var currSite in serverManager.Sites)
+                {
+                    var applicationRoot =
+                         currSite.Applications.Where(a => a.Path == "/").Single();
+                    var virtualRoot =
+                             applicationRoot.VirtualDirectories.Where(v => v.Path == "/").Single();
+                    var removeLastBar = virtualRoot.PhysicalPath.EndsWith(@"\") ? virtualRoot.PhysicalPath.Remove(virtualRoot.PhysicalPath.Length - 1, 1) : virtualRoot.PhysicalPath;
+                    var finalPath = removeLastBar.Split('\\').Last();
+                    Console.WriteLine(finalPath) ;
+                }
+                Console.WriteLine("");
+                Console.WriteLine("Pools");
+                Console.WriteLine("");
+                foreach (var appPool in new ServerManager().ApplicationPools)
+                {
+                    Console.WriteLine(appPool.Name);
+                }
+
+                Console.ReadKey();
+            }
         }
     }
 }
